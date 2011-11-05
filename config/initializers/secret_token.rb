2ed54e353178e20860b7cb3241d14b00bad7c8cb
Tad Thorley <phaedryx@gmail.com>
2011-11-04 19:41:55 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpineRailsTree::Application.config.secret_token = 'bbbe9f7642841aa4692a85ac00ddaa7b0bf0c4703526e78ffe7f71e40cf68ec30f3f9ec3e283e8417056ca1e7257434dc58c36897492729d275927c91cd69bab'
